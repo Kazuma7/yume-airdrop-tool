5a5c0fbc01a774182745fb909091c5bf81b209c8
Kazuma.I <kazuma.work77@gmail.com>
2022-08-22 13:48:56 +0900

    fix: 修正

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { ethers, Wallet } from "ethers";
 import { useEffect, useState } from "react";
 import Modal from "react-modal";
+import MintPage from "./components/MintPage";
 
 Modal.setAppElement("body");
 const customStyles = {
@@ -181,26 +182,7 @@ function App() {
             アドレスの確認
           </div>
         </div>
-        <div className="theme-card-bg rounded-xl mt-24 max-w-[360px] mx-auto">
-          <div className="text-2xl font-bold pt-10">限定NFT</div>
-          <div className="font-bold">作品タイトル</div>
-          <div className="flex justify-center mt-8">
-            <div className=" h-32 w-48">
-              <img src=" ./sample.jpeg" layout="fill" />
-            </div>
-          </div>
-          <div className="px-10 text-left mt-12 pb-12 text-sm">
-            花や植物を正確に、かつ細密に描き、それまでの植物画とはまったく異なる美しい作品を世に送り出したのが、彼ルドゥーテでした。革命の嵐が吹き荒れる激動期のパリに生きたその生涯をたどる
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <a
-          className="border rounded-full px-10 py-4 bg-green-500 text-white font-bold mt-16 cursor-pointer"
-          onClick={doMint}
-        >
-          限定NFTを受け取る
-        </a>
+        <MintPage doMint={doMint} />
       </div>
     </div>
   );
